19ab552fc0870be529274eba1db209df9f937730
brtrx <brtrx@users.noreply.github.com>
2026-01-02 15:54:39 +1100

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/trait-generator">
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/editor" element={<Index />} />
